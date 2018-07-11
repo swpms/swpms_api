9528c12c75482635e54dddc631fad2cc4150d5bf
Vo Anh Kiet <zvoanhkietz@gmail.com>
2018-07-11 22:08:11 +0700

    [add] config db

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,12 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// Database connection
+$container['db'] = function ($c) {
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($c['settings']['db']);
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+    return $capsule;
+};
